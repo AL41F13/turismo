42710e5a19e5f30c84328ae17d1ae0c5a819b9d2
AL41F13 <janeth982210@gmail.com>
2015-06-25 12:09:18 -0500

    tres
    js

---
+++ js/acciones.js
@@ -0,0 +1,137 @@
+// JavaScript Document
+$(document).ready(function(e) {
+document.addEventListener("deviceready",function(){
+	$('#menu').on ('tap', function(){
+		navigator.vibrate(1000);
+	});
+	$('#unopara').on ('tap', function(){
+		navigator.vibrate(1000);
+		
+	});
+	$('#doscozu').on ('tap', function(){
+		navigator.vibrate(1000);
+	});
+	$('#trescarmen').on ('tap', function(){
+		navigator.vibrate(1000);
+	});
+	$('#cuatrochetu').on ('tap', function(){
+		navigator.vibrate(1000);
+	});
+	$('#cincocan').on ('tap', function(){
+		navigator.vibrate(1000);
+	}); // esto es para los botones del menu
+	
+	$('#unoplayap').on ('tap',function (){//botones de inicio
+		navigator.vibrate (1000)
+	});
+	$('#doscozumelp').on ('tap',function (){
+		navigator.vibrate (1000)
+	});
+	$('#tresplayacarmen').on ('tap',function (){
+		navigator.vibrate (1000)
+	});
+	$('#cuatrochetumal4').on ('tap',function (){
+		navigator.vibrate (1000)
+	});
+	$('#cincocancun5').on ('tap',function (){
+		navigator.vibrate (1000)
+	});
+	//p
+	$('#t1').on ('tap',function (){
+		navigator.vibrate (1000)
+	});
+	$('#t2').on ('tap',function (){
+		navigator.vibrate (1000)
+	});
+	$('#t3').on ('tap',function (){
+		navigator.vibrate (1000)
+	});
+	$('#t4').on ('tap',function (){
+		navigator.vibrate (1000)
+	});
+	$('#t5').on ('tap',function (){
+		navigator.vibrate (1000)
+	});
+	//r
+	$('#r1').on ('tap',function (){
+		navigator.vibrate (1000)
+	});
+	$('#r2').on ('tap',function (){
+		navigator.vibrate (1000)
+	});
+	$('#r3').on ('tap',function (){
+		navigator.vibrate (1000)
+	});
+	$('#r4').on ('tap',function (){
+		navigator.vibrate (1000)
+	});
+	$('#r4').on ('tap',function (){
+		navigator.vibrate (1000)
+	});
+	//e
+	$('#e1').on ('tap',function (){
+		navigator.vibrate (1000)
+	});
+		$('#e2').on ('tap',function (){
+		navigator.vibrate (1000)
+	});
+		$('#e3').on ('tap',function (){
+		navigator.vibrate (1000)
+	});
+		$('#e4').on ('tap',function (){
+		navigator.vibrate (1000)
+	});
+		$('#e4').on ('tap',function (){
+		navigator.vibrate (1000)
+	});
+	//w
+		$('#w1').on ('tap',function (){
+		navigator.vibrate (1000)
+	});
+	$('#w2').on ('tap',function (){
+		navigator.vibrate (1000)
+	});
+	$('#w3').on ('tap',function (){
+		navigator.vibrate (1000)
+	});
+	$('#w4').on ('tap',function (){
+		navigator.vibrate (1000)
+	});
+	$('#w5').on ('tap',function (){
+		navigator.vibrate (1000)
+	});
+	//q
+	$('#q1').on ('tap',function (){
+		navigator.vibrate (1000)
+	});
+	$('#q2').on ('tap',function (){
+		navigator.vibrate (1000)
+	});
+	$('#q3').on ('tap',function (){
+		navigator.vibrate (1000)
+	});
+	$('#q4').on ('tap',function (){
+		navigator.vibrate (1000)
+	});
+	$('#q5').on ('tap',function (){
+		navigator.vibrate (1000)
+	});
+	//p
+	$('#p1').on ('tap',function (){
+		navigator.vibrate (1000)
+	});
+	$('#p2').on ('tap',function (){
+		navigator.vibrate (1000)
+	});
+	$('#p3').on ('tap',function (){
+		navigator.vibrate (1000)
+	});
+	$('#p4').on ('tap',function (){
+		navigator.vibrate (1000)
+	});
+	$('#p5').on ('tap',function (){
+		navigator.vibrate (1000)
+	});
+ }); 
+ });
+
